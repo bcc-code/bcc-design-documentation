1ca014dddccf57775b69f11517c866c7ddecd003
Michal Kupczak <kurczak12333@gmail.com>
2022-11-29 08:21:42 +0100

    feat: Implemented styling for components

--- componentsPackage/docs/.vuepress/config.js
+++ componentsPackage/docs/.vuepress/config.js
@@ -1,6 +1,8 @@
 import { defineUserConfig } from 'vuepress'
 import { getDirname, path } from '@vuepress/utils'
 import { registerComponentsPlugin } from '@vuepress/plugin-register-components'
+import tailwindcss from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
 
 const __dirname = getDirname(import.meta.url)
 
@@ -13,4 +15,14 @@ export default defineUserConfig({
       componentsDir: path.resolve(__dirname, './components'),
     }),
   ],
+  bundlerConfig: {
+    viteOptions: {
+      css: {
+        postcss: {
+          plugins: [tailwindcss, autoprefixer],
+        },
+      },
+    },
+  },
 })
+
