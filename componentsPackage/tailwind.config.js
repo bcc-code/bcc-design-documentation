c01d3ba866c3e2308aba64126d51812c30ce118e
Michal Kupczak <kurczak12333@gmail.com>
2022-12-07 11:08:41 +0100

    fix: Fixed flowbite flow

--- componentsPackage/tailwind.config.js
+++ componentsPackage/tailwind.config.js
@@ -3,10 +3,10 @@ module.exports = {
     './docs/.vitepress/**/*.{js,ts,vue}',
     './docs/**/*.{js,ts,vue,md}',
     './src/**/*.{js,ts,vue}',
-    'node_modules/flowbite-vue/**/*.{js,jsx,ts,tsx}',
-    'node_modules/flowbite/**/*.{js,jsx,ts,tsx}',
+    // 'node_modules/flowbite-vue/**/*.{js,jsx,ts,tsx}',
+    // 'node_modules/flowbite/**/*.{js,jsx,ts,tsx}',
     './node_modules/vuepress-theme-bcc-common-components/**/*.{js,ts,vue,md}',
   ],
-  plugins: [require('flowbite/plugin')],
+  // plugins: [require('flowbite/plugin')],
   theme: {},
 }
