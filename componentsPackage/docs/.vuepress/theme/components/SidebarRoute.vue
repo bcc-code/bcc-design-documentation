349ca969cbeed90ea007f6f70dfd27db23bae2af
Michal Kupczak <kurczak12333@gmail.com>
2022-12-12 15:46:40 +0100

    feat: Created a poc of sidebar component

--- componentsPackage/docs/.vuepress/theme/components/SidebarRoute.vue
+++ componentsPackage/docs/.vuepress/theme/components/SidebarRoute.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
+import { computed } from 'vue'
 import { ref } from 'vue'
-
+import { useRouter } from 'vue-router'
 const showChildren = ref(false)
+const router = useRouter()
 const props = defineProps<{
   route: any
   smallMargin: boolean | undefined
@@ -9,20 +11,35 @@ const props = defineProps<{
 
 console.log(props.route)
 
-const changeChildrenVisiblity = () => {
+const navigateToPage = () => {
   showChildren.value = !showChildren.value
+  if (props.route.link) {
+    router.push({ path: props.route.link })
+  }
 }
+
+//Check if actual url includes props.route.link
+const isActive = computed(() => {
+  if (props.route.link && router.currentRoute.value.path) {
+    //Remove index substring from link
+    let modifiedRoute = props.route.link
+    if (props.route.link.includes('index')) {
+      modifiedRoute = props.route.link.replace('index', '')
+    }
+    return router.currentRoute.value.path.split('.')[0].includes(modifiedRoute.split('.')[0])
+  }
+})
 </script>
 
 <template>
-  <div :style="{ marginBottom: props.smallMargin ? '1rem' : '2rem' }" class="item-wrapper">
-    <div class="item">
+  <div :style="{ marginBottom: props.smallMargin ? '1rem' : '2rem', cursor: props.route.link ? 'pointer' : 'auto' }" class="item-wrapper">
+    <div class="item" @click="navigateToPage">
       <i>
         <slot name="header"></slot>
       </i>
-      <div class="content">
+      <div class="content" :style="{ color: isActive ? 'var(--c-text-accent)' : 'var(--c-text)' }">
         <slot></slot>
-        <div v-if="props.route.children" @click="changeChildrenVisiblity">></div>
+        <div v-if="props.route.children">></div>
       </div>
     </div>
     <div v-if="props.route.children && showChildren">
