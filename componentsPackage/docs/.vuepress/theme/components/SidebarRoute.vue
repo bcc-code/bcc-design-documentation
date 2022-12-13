61f79fdafb3afff223706163c48de6b25a8a94ff
Michal Kupczak <kurczak12333@gmail.com>
2022-12-13 16:05:27 +0100

    fix: Improved arrows logic

--- componentsPackage/docs/.vuepress/theme/components/SidebarRoute.vue
+++ componentsPackage/docs/.vuepress/theme/components/SidebarRoute.vue
@@ -54,8 +54,13 @@ const uppercaseFirstLetter = (text: string) => {
       <span class="flex-1 ml-3 text-left whitespace-nowrap" :style="{ fontWeight: isActive ? 700 : 400, color: isActive ? 'var(--c-text-accent)' : 'var(--c-text)' }">{{
         uppercaseFirstLetter(props.route.text)
       }}</span>
-      <svg v-if="props.route.children && props.route.children.length > 0" aria-hidden="true" class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-        <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+      <!-- Down arrow -->
+      <svg v-if="props.route.children && props.route.children.length > 0 && !showChildren" class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 28 28" xmlns="http://www.w3.org/2000/svg">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
+      </svg>
+      <!-- Right arrow -->
+      <svg v-if="props.route.children && props.route.children.length > 0 && showChildren" class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 28 28" xmlns="http://www.w3.org/2000/svg">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path>
       </svg>
     </button>
     <div v-if="props.route.children && showChildren">
