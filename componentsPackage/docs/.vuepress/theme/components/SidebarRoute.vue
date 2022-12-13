817a4e0c8a4507908e997d68b2293e66067907b2
Michal Kupczak <kurczak12333@gmail.com>
2022-12-13 15:53:37 +0100

    fix: Improved styling and highlighting

--- componentsPackage/docs/.vuepress/theme/components/SidebarRoute.vue
+++ componentsPackage/docs/.vuepress/theme/components/SidebarRoute.vue
@@ -51,7 +51,9 @@ const uppercaseFirstLetter = (text: string) => {
       @click="navigateToPage"
     >
       <img v-if="pathIcon" :src="pathIcon" />
-      <span class="flex-1 ml-3 text-left whitespace-nowrap" :style="{ color: isActive ? 'var(--c-text-accent)' : 'var(--c-text)' }">{{ uppercaseFirstLetter(props.route.text) }}</span>
+      <span class="flex-1 ml-3 text-left whitespace-nowrap" :style="{ fontWeight: isActive ? 700 : 400, color: isActive ? 'var(--c-text-accent)' : 'var(--c-text)' }">{{
+        uppercaseFirstLetter(props.route.text)
+      }}</span>
       <svg v-if="props.route.children && props.route.children.length > 0" aria-hidden="true" class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
         <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path>
       </svg>
