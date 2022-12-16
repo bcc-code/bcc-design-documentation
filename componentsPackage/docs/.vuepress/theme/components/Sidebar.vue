3fd475915a5d392defb9adabb6b7e7a9447d7f51
Michal Kupczak <kurczak12333@gmail.com>
2022-12-16 11:04:59 +0100

    feat: Improved colours, navbar indentations and default top layer nav item icon

--- componentsPackage/docs/.vuepress/theme/components/Sidebar.vue
+++ componentsPackage/docs/.vuepress/theme/components/Sidebar.vue
@@ -7,7 +7,7 @@ const props = defineProps(['routes', 'pathIcons'])
   <aside class="fixed top-0 left-0 w-80 h-full sidebar custom-margin" id="sidebarWrapper" aria-label="Sidenav">
     <div class="overflow-y-auto py-5 px-3 h-full bg-white border-r border-gray-200 hidden-padding" id="sidebar">
       <ul class="space-y-2">
-        <SidebarRoute v-for="item in props.routes" :key="item.text" :route="item" :pathIcons="pathIcons">
+        <SidebarRoute v-for="item in props.routes" :key="item.text" :route="item" :pathIcons="pathIcons" :showIndentation="false" :isTopLevel="true">
           {{ item.text }}
         </SidebarRoute>
       </ul>
@@ -18,6 +18,7 @@ const props = defineProps(['routes', 'pathIcons'])
 @media only screen and (min-width: 720px) {
   .custom-margin {
     margin-top: 58px;
+    padding-bottom: 58px;
   }
 }
 </style>
--- componentsPackage/docs/.vuepress/theme/components/SidebarRoute.vue
+++ componentsPackage/docs/.vuepress/theme/components/SidebarRoute.vue
@@ -2,11 +2,14 @@
 import { computed } from 'vue'
 import { ref } from 'vue'
 import { useRouter } from 'vue-router'
+
 const showChildren = ref(false)
 const router = useRouter()
 const props = defineProps<{
   route: any
   pathIcons: any
+  showIndentation: boolean
+  isTopLevel: boolean
 }>()
 
 const navigateToPage = () => {
@@ -47,14 +50,24 @@ const uppercaseFirstLetter = (text: string) => {
 </script>
 
 <template>
-  <div>
+  <div :style="{ paddingLeft: props.showIndentation ? '8px' : '6px' }">
     <button
       type="button"
       class="flex items-center p-2 w-full text-base font-normal text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
       @click="navigateToPage"
     >
       <div class="h-full w-6">
-        <img v-if="pathIcon" :src="pathIcon" />
+        <img v-if="props.isTopLevel && pathIcon" :src="pathIcon" />
+        <svg v-if="props.isTopLevel && !pathIcon" width="16" height="21" viewBox="0 0 16 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path
+            d="M11.5 3C12.9045 3 13.6067 3 14.1111 3.33706C14.3295 3.48298 14.517 3.67048 14.6629 3.88886C15 4.39331 15 5.09554 15 6.5V16C15 17.8856 15 18.8284 14.4142 19.4142C13.8284 20 12.8856 20 11 20H5C3.11438 20 2.17157 20 1.58579 19.4142C1 18.8284 1 17.8856 1 16V6.5C1 5.09554 1 4.39331 1.33706 3.88886C1.48298 3.67048 1.67048 3.48298 1.88886 3.33706C2.39331 3 3.09554 3 4.5 3"
+            stroke="#9F9F9F"
+            stroke-width="2"
+          />
+          <path d="M5 3C5 1.89543 5.89543 1 7 1H9C10.1046 1 11 1.89543 11 3C11 4.10457 10.1046 5 9 5H7C5.89543 5 5 4.10457 5 3Z" stroke="#9F9F9F" stroke-width="2" />
+          <path d="M5 10L11 10" stroke="#9F9F9F" stroke-width="2" stroke-linecap="round" />
+          <path d="M5 14L9 14" stroke="#9F9F9F" stroke-width="2" stroke-linecap="round" />
+        </svg>
       </div>
       <span class="flex-1 ml-3 text-left whitespace-nowrap" :style="{ fontWeight: isActive ? 700 : 400, color: isActive ? 'var(--c-text-accent)' : 'var(--c-text)' }">{{
         uppercaseFirstLetter(props.route.text)
@@ -69,7 +82,7 @@ const uppercaseFirstLetter = (text: string) => {
       </svg>
     </button>
     <div v-if="props.route.children && showChildren">
-      <SidebarRoute v-for="child in props.route.children" :key="child.id" :route="child" :pathIcons="props.pathIcons">
+      <SidebarRoute v-for="child in props.route.children" :key="child.id" :route="child" :pathIcons="props.pathIcons" :showIndentation="true" :isTopLevel="false">
         {{ child.text }}
       </SidebarRoute>
     </div>
